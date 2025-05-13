58ba2b060fdcabae43933879e2fe1b68e386946b
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-05-13 16:09:37 +0000

    Add files via upload

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,9 @@
+SELECT properties.property_id, properties.name, avg_ratings.average_rating
+FROM airbnb_clone.properties
+JOIN (
+    SELECT reviews.property_id, AVG(rating) as average_rating
+    FROM airbnb_clone.reviews
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+) avg_ratings ON properties.property_id = avg_ratings.property_id
+;
