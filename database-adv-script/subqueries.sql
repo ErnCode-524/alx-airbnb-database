e1ce61003505dfda2d445cf867d7741a6aef49ea
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-05-13 20:33:42 +0000

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -8,3 +8,11 @@ JOIN (
 ) avg_ratings ON properties.property_id = avg_ratings.property_id
     WHERE average_rating > 4.0
 ;
+
+SELECT user_id, first_name, last_name
+FROM airbnb_clone.Users u
+WHERE (
+    SELECT COUNT(*)
+    FROM airbnb_clone.Bookings b
+    WHERE b.user_id = u.user_id
+) > 3;
