defc0c8cc2b9a6e7e004baae45ae89f390bff850
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-05-13 14:32:10 +0000

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,24 +1,21 @@
-select * 
-from airbnb_clone.users
-INNER JOIN airbnb_clone.bookings
-on users.user_id = bookings.user_id
-;
+SELECT * 
+FROM airbnb_clone.Users
+INNER JOIN airbnb_clone.Bookings
+ON Users.user_id = Bookings.user_id;
 
-select * 
-from airbnb_clone.properties
-LEFT JOIN airbnb_clone.reviews
-on properties.property_id = reviews.property_id
-;
+SELECT * 
+FROM airbnb_clone.Properties
+LEFT JOIN airbnb_clone.Reviews
+ON Properties.property_id = Reviews.property_id;
 
-select * 
-from airbnb_clone.users
-LEFT JOIN airbnb_clone.bookings
-on users.user_id = bookings.user_id
+SELECT * 
+FROM airbnb_clone.Users
+LEFT JOIN airbnb_clone.Bookings
+ON Users.user_id = Bookings.user_id
 
 UNION
 
-select * 
-from airbnb_clone.users
-RIGHT JOIN airbnb_clone.bookings
-on users.user_id = bookings.user_id
-;
+SELECT * 
+FROM airbnb_clone.Users
+RIGHT JOIN airbnb_clone.Bookings
+ON Users.user_id = Bookings.user_id;
