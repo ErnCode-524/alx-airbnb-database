8e8ca4ab80b40d83fec3931671824e93ee69148e
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-05-14 12:58:23 +0000

    Create database_index.sql

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,22 @@
+SELECT * 
+FROM airbnb_clone.users;
+
+SELECT * 
+FROM airbnb_clone.bookings;
+
+SELECT * 
+FROM airbnb_clone.properties;
+
+EXPLAIN SELECT * 
+FROM airbnb_clone.users
+WHERE user_id = 'BZ524';
+
+CREATE INDEX usid_index ON users(user_id);
+CREATE INDEX bookid_index ON bookings(booking_id);
+CREATE INDEX properties_index ON properties(name);
+
+EXPLAIN SELECT * 
+FROM airbnb_clone.users
+WHERE user_id = 'BZ524';
+
+SHOW INDEX FROM properties;
