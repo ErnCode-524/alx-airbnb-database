6141bf6347bd000178a0fb421cb8136d3dded575
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-05-15 14:11:51 +0000

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -7,3 +7,15 @@ ON prop.property_id = book.property_id
 JOIN airbnb_clone.payments AS pay
 ON book.booking_id = pay.booking_id
 ;
+
+-- WHERE, AND
+
+EXPLAIN ANALYZE SELECT * 
+FROM airbnb_clone.users AS users
+JOIN airbnb_clone.properties AS prop
+ON users.user_id = prop.host_id
+JOIN airbnb_clone.bookings AS book
+ON prop.property_id = book.property_id
+JOIN airbnb_clone.payments AS pay
+ON book.booking_id = pay.booking_id
+;
